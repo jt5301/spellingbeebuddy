6947eadd953854115c50644141329059c4635976
John Tieu <johnson.tieu@gmail.com>
2020-12-30 01:38:02 -0500

    imported fonts

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './main/App';
 import reportWebVitals from './reportWebVitals';
+import Header from './Header'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header/>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
